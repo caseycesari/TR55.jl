5bc2db40da4aa47c85128cb2b9785f8a175b833e
Casey Thomas <cpthomas@gmail.com>
2015-03-22 18:19:24 -0400

    Rename functions

--- src/TR55.jl
+++ src/TR55.jl
@@ -3,8 +3,8 @@ module TR55
 # Calculates the runoff (in) based off
 # s (potential maximum retention after runoff begins)
 # p (rainfall (in))
-function q(s, p)
-    Q = (p - (0.2 * s))^2 / (p + (0.8 * s))
+function runoff(S, P)
+    Q = (P - (0.2 * S))^2 / (P + (0.8 * S))
 
     return round(Q, 2)
 end
@@ -12,8 +12,8 @@ end
 # Calculates the potential maximum retention
 # after runoff begins based off
 # cn (runoff curve number)
-function s(cn)
-    S = (1000 / cn) - 10
+function max_retention(CN)
+    S = (1000 / CN) - 10
 
     return round(S, 2)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,25 +5,25 @@ using Base.Test
 # http://www.nrcs.usda.gov/Internet/FSE_DOCUMENTS/stelprdb1044171.pdf
 
 # Figure 2-5
-figure_25_s = TR55.s(70)
-figure_25_q = TR55.q(figure_25_s, 6)
+figure_25_s = TR55.max_retention(70)
+figure_25_q = TR55.runoff(figure_25_s, 6)
 @test figure_25_s == 4.29
 @test figure_25_q == 2.8
 
 # Figure 2-6
-figure_26_s = TR55.s(75)
-figure_26_q = TR55.q(figure_26_s, 6)
+figure_26_s = TR55.max_retention(75)
+figure_26_q = TR55.runoff(figure_26_s, 6)
 @test figure_26_s == 3.33
 @test figure_26_q == 3.28
 
 # Figure 2-7
-figure_27_s = TR55.s(77)
-figure_27_q = TR55.q(figure_27_s, 6)
+figure_27_s = TR55.max_retention(77)
+figure_27_q = TR55.runoff(figure_27_s, 6)
 @test figure_27_s == 2.99
 @test figure_27_q == 3.48
 
 # Figure 2-8
-figure_28_s = TR55.s(74)
-figure_28_q = TR55.q(figure_28_s, 6)
+figure_28_s = TR55.max_retention(74)
+figure_28_q = TR55.runoff(figure_28_s, 6)
 @test figure_28_s == 3.51
 @test figure_28_q == 3.19
