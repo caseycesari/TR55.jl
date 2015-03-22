aae0edb0829df98ce79865dc03463900ce3b488f
Casey Thomas <cpthomas@gmail.com>
2015-03-22 18:30:13 -0400

    Assert types for functions

--- src/TR55.jl
+++ src/TR55.jl
@@ -1,18 +1,18 @@
 module TR55
 
-# Calculates the runoff (in) based off
-# s (potential maximum retention after runoff begins)
-# p (rainfall (in))
-function runoff(S, P)
+# Calculates the runoff (in) based off:
+# S (potential maximum retention after runoff begins)
+# P (rainfall (in))
+function runoff(S::Number, P::Number)
     Q = (P - (0.2 * S))^2 / (P + (0.8 * S))
 
     return round(Q, 2)
 end
 
 # Calculates the potential maximum retention
-# after runoff begins based off
-# cn (runoff curve number)
-function max_retention(CN)
+# after runoff begins based off:
+# CN (runoff curve number)
+function max_retention(CN::Int64)
     S = (1000 / CN) - 10
 
     return round(S, 2)
