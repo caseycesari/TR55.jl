a5a782708dd666d75122816959998e9a9063d0d2
Casey Thomas <cpthomas@gmail.com>
2015-03-22 19:55:56 -0400

    Add method for weighted curve number

--- src/TR55.jl
+++ src/TR55.jl
@@ -18,4 +18,36 @@ function max_retention(CN::Int64)
     return round(S, 2)
 end
 
+# Calculates the weighted curve number
+# for a watershed.
+# region - array of dicts w/ the following structure:
+#   [
+#       {
+#           "cn" => 61,
+#           "area" => 30
+#       },
+#       {
+#           "cn" => 74,
+#           "area" => 70
+#       },
+#   ]
+# area_is_percent - boolean indicating whether or not
+# the region areas are measured in percent of area,
+# instead of acres or sq mi, etc.
+function weighted_cn(regions::Array{Dict{Any,Any}},
+                     area_is_percent::Bool)
+
+    total_area = reduce(+, map(x -> x["area"], regions))
+
+    if area_is_percent && total_area != 100
+        error("Using percents areas requires that " *
+              "region areas sum to 100")
+    end
+
+    total_product = reduce(+, map(x -> x["area"] * x["cn"], regions))
+    weighted_CN = total_product / total_area
+
+    return convert(Int64, round(weighted_CN))
+end
+
 end # module
